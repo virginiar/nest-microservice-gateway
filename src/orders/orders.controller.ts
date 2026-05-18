5bf13ae6fd1150df10d212f34bac06dbd5990a10
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-05-18 14:51:10 +0200

    Implementando findOne para Order

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get, Post, Body, Param, Inject } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Inject,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { ORDER_SERVICE } from '../config';
 import { CreateOrderDto } from './dto/create-order.dto';
+import { catchError, firstValueFrom } from 'rxjs';
 
 @Controller('orders')
 export class OrdersController {
@@ -20,7 +29,20 @@ export class OrdersController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ordersClient.send('findOneOrder', { id });
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.ordersClient.send('findOneOrder', { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+    // try {
+    //   const order = await firstValueFrom(
+    //     this.ordersClient.send('findOneOrder', { id }),
+    //   );
+
+    //   return order;
+    // } catch (error) {
+    //   throw new RpcException(error);
+    // }
   }
 }
