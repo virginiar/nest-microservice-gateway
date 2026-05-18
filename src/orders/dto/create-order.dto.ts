508cdda2f4531f5a75744ae1fbe64f7078c4902c
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-05-18 13:23:28 +0200

    Implementando la creación de órdenes

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,28 @@
-export class CreateOrderDto {}
+import {
+  IsNumber,
+  IsPositive,
+  IsEnum,
+  IsOptional,
+  IsBoolean,
+} from 'class-validator';
+import { OrderStatusList, OrderStatus } from '../enum/order.enum';
+
+export class CreateOrderDto {
+  @IsNumber()
+  @IsPositive()
+  totalAmount!: number;
+
+  @IsNumber()
+  @IsPositive()
+  totalItems!: number;
+
+  @IsEnum(OrderStatusList, {
+    message: `Possible status values are ${OrderStatusList}`,
+  })
+  @IsOptional()
+  status: OrderStatus = OrderStatus.PENDING;
+
+  @IsBoolean()
+  @IsOptional()
+  paid: boolean = false;
+}
