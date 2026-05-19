c0ded364a2718c4a98954d6eeff35c5f9f852248
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-05-19 15:55:42 +0200

    Refactorizando CreateOrderDto y añadiendo OrderItemDto

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,28 +1,11 @@
-import {
-  IsNumber,
-  IsPositive,
-  IsEnum,
-  IsOptional,
-  IsBoolean,
-} from 'class-validator';
-import { OrderStatusList, OrderStatus } from '../enum/order.enum';
+import { Type } from 'class-transformer';
+import { ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
+import { OrderItemDto } from '.';
 
 export class CreateOrderDto {
-  @IsNumber()
-  @IsPositive()
-  totalAmount!: number;
-
-  @IsNumber()
-  @IsPositive()
-  totalItems!: number;
-
-  @IsEnum(OrderStatusList, {
-    message: `Possible status values are ${OrderStatusList}`,
-  })
-  @IsOptional()
-  status: OrderStatus = OrderStatus.PENDING;
-
-  @IsBoolean()
-  @IsOptional()
-  paid: boolean = false;
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items!: OrderItemDto[];
 }
