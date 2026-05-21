4a61d3d3bcd78fcff08d01584fad545b6b2bcbcd
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-05-21 12:28:17 +0200

    Creando módulo personalizado NatsModule

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,21 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { envs, NATS_SERVICE } from '../config';
 import { ProductsController } from './products.controller';
+import { NatsModule } from '../transport/nats.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [],
-  imports: [
-    ClientsModule.register([
-      {
-        name: NATS_SERVICE,
-        transport: Transport.NATS,
-        options: {
-          servers: envs.natsServers,
-        },
-      },
-    ]),
-  ],
+  imports: [NatsModule],
 })
 export class ProductsModule {}
